cf926e366f465ea971d6d2abf03c8a07923f486b
¨noutaylanefzaoui¨ <noutaylanefzaoui@outlook.fr>
2025-02-27 15:01:06 +0100

    W3 List in React

---
+++ src/App.jsx
@@ -0,0 +1,53 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import * as React from "react";
+const list = [
+  {
+    title: "React",
+    url: "https://reactjs.org/",
+    author: "Jordan Walke",
+    num_comments: 3,
+    points: 4,
+    objectID: 0,
+  },
+  {
+    title: "Redux",
+    url: "https://redux.js.org/",
+    author: "Dan Abramov, Andrew Clark",
+    num_comments: 2,
+    points: 5,
+    objectID: 1,
+  },
+];
+function getTitle(title) {
+  return title;
+}
+function App(){
+  return (
+    <div>
+      <h1>Hello {getTitle("React")}</h1>
+      <label htmlFor="search">Search: </label>
+      <input type="text" id ="search" />
+      <hr />
+      <ul>
+        {list.map(function(item){
+          return (
+          <li key={item.objectID}>
+            <span>
+              <a href={item.url}>{item.title}</a>
+            </span>
+            <span>{item.author}</span>
+            <span>{item.num_comments}</span>
+            <span>{item.points}</span>
+          </li>
+          );
+        
+        })}
+      </ul>
+    </div>
+  );
+}
+
+export default App
