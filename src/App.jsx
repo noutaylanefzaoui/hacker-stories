dcf4cc74c1893c877d67226f10271a3aecf28682
¨noutaylanefzaoui¨ <noutaylanefzaoui@outlook.fr>
2025-04-10 14:34:04 +0100

    W4 React Multiple Components

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,7 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import * as React from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+
 const list = [
   {
     title: "React",
@@ -14,40 +13,61 @@ const list = [
   },
   {
     title: "Redux",
-    url: "https://redux.js.org/",
-    author: "Dan Abramov, Andrew Clark",
+    url: "https://redux.js.org",
+    author: "Dan Abramov and Andrew Clark",
     num_comments: 2,
     points: 5,
     objectID: 1,
-  },
+  }
 ];
-function getTitle(title) {
-  return title;
-}
-function App(){
+
+function Search() {
   return (
     <div>
-      <h1>Hello {getTitle("React")}</h1>
       <label htmlFor="search">Search: </label>
-      <input type="text" id ="search" />
-      <hr />
-      <ul>
-        {list.map(function(item){
-          return (
+      <input id="search" type="text" />
+    </div>
+  );
+}
+
+function List({ list }) {
+  return (
+    <ul>
+      {list.map(function (item) {
+        return (
           <li key={item.objectID}>
             <span>
-              <a href={item.url}>{item.title}</a>
+              <a href={item.url}> {item.title}</a>
             </span>
-            <span>{item.author}</span>
-            <span>{item.num_comments}</span>
-            <span>{item.points}</span>
+            <span> Author: {item.author}</span>
+            <span> | Comments: {item.num_comments}</span>
+            <span> | Points: {item.points}</span>
           </li>
-          );
-        
-        })}
+        );
+      })}
+    </ul>
+  );
+}
+
+function getTitle(title) {
+  return `Title: ${title}`;
+}
+
+function App() {
+  return (
+    <div>
+      <h1>My Hacker Stories</h1>
+      <Search />
+      <hr />
+      <List list={list} />
+      <h2>Titles :</h2>
+      <ul>
+        {list.map((item) => (
+          <li key={item.objectID}>{getTitle(item.title)}</li>
+        ))}
       </ul>
     </div>
   );
 }
 
-export default App
+export default App;
