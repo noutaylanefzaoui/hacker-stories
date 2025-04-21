c69809be75148c0f2403c3c65cc1354a3e1aa7d8
¨noutaylanefzaoui¨ <noutaylanefzaoui@outlook.fr>
2025-04-21 14:23:21 +0100

    W6 Adding React Props

--- src/App.jsx
+++ src/App.jsx
@@ -2,24 +2,7 @@ import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
 
-const list = [
-  {
-    title: "React",
-    url: "https://reactjs.org/",
-    author: "Jordan Walke",
-    num_comments: 3,
-    points: 4,
-    objectID: 0,
-  },
-  {
-    title: "Redux",
-    url: "https://redux.js.org",
-    author: "Dan Abramov and Andrew Clark",
-    num_comments: 2,
-    points: 5,
-    objectID: 1,
-  }
-];
+
 
 const Search = () => {
   const handleChange = (event) => {
@@ -34,38 +17,62 @@ const Search = () => {
   );
 };
 
-const List = () => (
+const List = (props) => (
     <ul>
-      {list.map((item) => (
-          <li key={item.objectID}>
-            <span>
-              <a href={item.url}> {item.title}</a>
-            </span>
-            <span> Author: {item.author}</span>
-            <span> | Comments: {item.num_comments}</span>
-            <span> | Points: {item.points}</span>
-          </li>
+      {props.list.map((item) => (
+          <Item key={item.objectID} item={item} />
       ))}
     </ul>
+  );
+  const Item = (props) => (
+    <li>
+      <span>
+        <a href={props.item.url}> {props.item.title}</a>
+      </span>
+      <span> Author : {props.item.author}</span>
+      <span> | Comments : {props.item.num_comments}</span>
+      <span> | Points : {props.item.points}</span>
+    </li>
   );
 
 function getTitle(title) {
   return `Title: ${title}`;
 }
 
-const App = () => (
+const App = () => {
+  const stories = [
+    {
+      title: "React",
+      url: "https://reactjs.org/",
+      author: "Jordan Walke",
+      num_comments: 3,
+      points: 4,
+      objectID: 0,
+    },
+    {
+      title: "Redux",
+      url: "https://redux.js.org",
+      author: "Dan Abramov and Andrew Clark",
+      num_comments: 2,
+      points: 5,
+      objectID: 1,
+    }
+  
+  ];
+  return (
     <div>
       <h1>My Hacker Stories</h1>
       <Search />
       <hr />
-      <List list={list} />
+      <List list={stories} />
       <h2>Titles :</h2>
       <ul>
-        {list.map((item) => (
+        {stories.map((item) => (
           <li key={item.objectID}>{getTitle(item.title)}</li>
         ))}
       </ul>
     </div>
   );
+};
 
 export default App;
