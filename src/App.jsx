ce63538b45b02c1c5778b158c300bcffeffbb1d2
¨noutaylanefzaoui¨ <noutaylanefzaoui@outlook.fr>
2025-05-15 22:50:58 +0100

    W8 reusable react component, react component composition, inline handler

--- src/App.jsx
+++ src/App.jsx
@@ -1,46 +1,10 @@
-import reactLogo from "./assets/react.svg";
+import React from "react"; // Import React
+import reactLogo from "./assets/react.svg"; // Tell Webpack to load this file
 import viteLogo from "/vite.svg";
 import "./App.css";
 
-
-
-const Search = () => {
-  const handleChange = (event) => {
-    console.log(event);
-    console.log(event.target.value);
-  };
-  return (
-    <div>
-      <label htmlFor="search">Search: </label>
-      <input id="search" type="text" onChange={handleChange} />
-    </div>
-  );
-};
-
-const List = (props) => (
-    <ul>
-      {props.list.map((item) => (
-          <Item key={item.objectID} item={item} />
-      ))}
-    </ul>
-  );
-  const Item = (props) => (
-    <li>
-      <span>
-        <a href={props.item.url}> {props.item.title}</a>
-      </span>
-      <span> Author : {props.item.author}</span>
-      <span> | Comments : {props.item.num_comments}</span>
-      <span> | Points : {props.item.points}</span>
-    </li>
-  );
-
-function getTitle(title) {
-  return `Title: ${title}`;
-}
-
 const App = () => {
-  const stories = [
+  const initialStories = [
     {
       title: "React",
       url: "https://reactjs.org/",
@@ -56,23 +20,87 @@ const App = () => {
       num_comments: 2,
       points: 5,
       objectID: 1,
-    }
-  
+    },
   ];
+  const [searchTerm, setSearchTerm] = React.useState(
+    localStorage.getItem("search") || "React"
+  );
+  const [stories, setStories] = React.useState(initialStories);
+  const handleRemoveStory = (item) => {
+    const newStories = stories.filter(
+      (story) => item.objectID !== story.objectID
+    );
+    setStories(newStories);
+  };
+
+  React.useEffect(() => {
+    localStorage.setItem("search", searchTerm);
+  }, [searchTerm]);
+
+  const handleSearch = (event) => {
+    setSearchTerm(event.target.value);
+  };
+  const searchedStories = stories.filter(function (story) {
+    return story.title.toLowerCase().includes(searchTerm.toLowerCase());
+  });
   return (
     <div>
       <h1>My Hacker Stories</h1>
-      <Search />
+      <InputWithLabel
+        id="search"
+        value={searchTerm}
+        onInputChange={handleSearch}
+      >
+        Search:
+      </InputWithLabel>
       <hr />
-      <List list={stories} />
-      <h2>Titles :</h2>
-      <ul>
-        {stories.map((item) => (
-          <li key={item.objectID}>{getTitle(item.title)}</li>
-        ))}
-      </ul>
+      <List list={searchedStories} onRemove={handleRemoveStory} />
     </div>
   );
 };
+
+const InputWithLabel = ({
+  id,
+  label,
+  value,
+  onInputChange,
+  type = "text",
+  children,
+}) => (
+  <>
+    <label htmlFor={id}>{children}</label>
+    &nbsp;
+    <input type={type} id={id} value={value} onChange={onInputChange} />
+  </>
+);
+
+const List = ({ list, onRemove }) => (
+  <ul>
+    {list.map((item) => (
+      <Item key={item.objectID} item={item} onRemove={onRemove} />
+    ))}
+  </ul>
+);
+
+const Item = ({ item, onRemove }) => (
+  <li>
+    <span>
+      <a href={item.url}> {item.title}</a>
+    </span>
+    <span> Author: {item.author}</span>
+    <span> | Comments: {item.num_comments}</span>
+    <span> | Points: {item.points}</span>
+    <span>
+      <button type="button" onClick={() => onRemove(item)}>
+        Remove
+      </button>
+    </span>
+  </li>
+);
+
+function getTitle(title) {
+  // function to format the title of an item
+  return `Title: ${title}`; // return a formatted string with the title
+}
 
 export default App;
